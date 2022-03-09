05ad20156d4a14e9f1ff98cab06ee8b17b51f6bc
taoufik ahjam <taoufikahjam@gmail.com>
2022-03-09 18:57:04 +0100

    add useEffect and Pokemon Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
 import Button from './components/Button';
 import { useState } from 'react';
+import Pokemon, {usePokemon} from './components/Pokemon';
+import { useEffect } from 'react';
 
 function App() {
    const [totalClick, setTotalClick] = useState(0)
-
+    const{pokemon,addPokemon}=usePokemon()
   const HandleTotalClick=()=>{
     setTotalClick(totalClick+1)
   }
-
+  useEffect(() => {
+    if (totalClick>0) {
+      addPokemon()
+    }
+  }, [totalClick])
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -16,7 +23,7 @@ function App() {
          <Button title='this is the frist button' handleButtonTotalClick={HandleTotalClick}  />
          <Button title='this is the second button' handleButtonTotalClick={HandleTotalClick} />
          <Button title='this is the thirth button' handleButtonTotalClick={HandleTotalClick} />
-
+         <Pokemon pokemon={pokemon}/>
       </header>
     </div>
   );
